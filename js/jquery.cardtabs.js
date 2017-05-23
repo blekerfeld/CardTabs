3b664842e351cd920ec7d9344646252698e9a86d
Thomas de Roo <t.r.c.de.roo@student.rug.nl>
2017-05-23 16:07:11 +0200

    update

--- js/jquery.cardtabs.js
+++ js/jquery.cardtabs.js
@@ -5,20 +5,30 @@
  * Released under the MIT license
  */
 
-jQuery.fn.cardTabs = function(){
+jQuery.fn.cardTabs = function(options){
 
 	var mainClass =  $(this).attr('class');
 	var activeCount = 0;
 
+	var settings = $.extend({
+        theme: '',
+     }, options );
+
+	// Fixing the theme
+	if(settings.theme != ''){
+		$('.' + mainClass + ' .card-tabs-bar').addClass(settings.theme);
+		$('.' + mainClass + ' .card-tabs-stack').addClass(settings.theme);
+	}
+
 	function toggleTab(obj){
-		$('.' + mainClass + " div[data-tab='" + obj.data('tab') + "']").show();
-		$('.' + mainClass + " div[data-tab!='" + obj.data('tab') + "']").hide();
+		$('.' + mainClass + " .card-tabs-stack div[data-tab][data-tab='" + obj.data('tab') + "']").show();
+		$('.' + mainClass + " .card-tabs-stack div[data-tab][data-tab!='" + obj.data('tab') + "']").hide();
 	}
 
 	// Checking whether we have to set a tab as active
-    $('.' + mainClass + ' .ctTabs').children('.ctTab').each(function () {
+    $('.' + mainClass + ' .card-tabs-stack').children('div[data-tab]').each(function () {
     	if($(this).hasClass('active')){
-    		$('.' + mainClass + " .ctBar a[data-tab='" + $(this).data('tab') + "']").addClass('active');
+    		$('.' + mainClass + " .card-tabs-bar a[data-tab='" + $(this).data('tab') + "']").addClass('active');
     		toggleTab($(this));
     		$(this).removeClass('active');
     		activeCount++;
@@ -27,11 +37,11 @@ jQuery.fn.cardTabs = function(){
 
 	// Otherwise, it's the first one, and the first tab in the bar needs to be active
 	if(activeCount == 0){
-		$('.' + mainClass + ' .ctBar a:first-child').addClass('active');
+		$('.' + mainClass + ' .card-tabs-bar a:first-child').addClass('active');
 	}
 
-	$('.' + mainClass + ' .ctBar a').click(function(){
-		$('.' + mainClass + ' .ctBar a').removeClass('active');
+	$('.' + mainClass + ' .card-tabs-bar a').click(function(){
+		$('.' + mainClass + ' .card-tabs-bar a').removeClass('active');
 		$(this).addClass('active');
 		toggleTab($(this));
 	});
