bb94428dc39df87b968a7829575bee2635f629ec
Thomas de Roo <t.r.c.de.roo@student.rug.nl>
2018-03-28 18:29:37 +0200

    Possible fix for #11

--- docs/jquery.cardtabs.js
+++ docs/jquery.cardtabs.js
@@ -7,7 +7,8 @@
 
 jQuery.fn.cardTabs = function(options){
 
-	var mainClass =  $(this).attr('class');
+	// Getting the initial selector
+	var selector = $(this).parents().map(function() { return this.tagName; }).get().reverse().concat([this.nodeName]).join(">");var id = $(this).attr("id");if (id) { selector += "#"+ id;}var classNames = $(this).attr("class");if (classNames) {selector += "." + $.trim(classNames).replace(/\s/gi, ".");}
 	var activeCount = 0;
 
 	var settings = $.extend({
@@ -19,28 +20,28 @@ jQuery.fn.cardTabs = function(options){
 	var stack = $('<div />').addClass('card-tabs-stack').html(htmlInner);
 	var bar = $('<div />').addClass('card-tabs-bar');
 
-	$('.' + mainClass).children('div[data-tab]').each(function(){
+	$(selector).children('div[data-tab]').each(function(){
 		bar.append($('<a />').attr('href', 'javascript:void();').data('tab', $(this).data('tab')).append($(this).data('tab')));
 	});
 
-	$('.' + mainClass).html('').append(bar).append(stack);
+	$(selector).html('').append(bar).append(stack);
 
 
 	// Fixing the theme
 	if(settings.theme != ''){
-		$('.' + mainClass + ' .card-tabs-bar').addClass(settings.theme);
-		$('.' + mainClass + ' .card-tabs-stack').addClass(settings.theme);
+		$(selector + ' .card-tabs-bar').addClass(settings.theme);
+		$(selector + ' .card-tabs-stack').addClass(settings.theme);
 	}
 
 	function toggleTab(obj){
-		$('.' + mainClass + " .card-tabs-stack div[data-tab][data-tab='" + obj.data('tab') + "']").show();
-		$('.' + mainClass + " .card-tabs-stack div[data-tab][data-tab!='" + obj.data('tab') + "']").hide();
+		$(selector + " .card-tabs-stack div[data-tab][data-tab='" + obj.data('tab') + "']").show();
+		$(selector + " .card-tabs-stack div[data-tab][data-tab!='" + obj.data('tab') + "']").hide();
 	}
 
 	// Checking whether we have to set a tab as active
-    $('.' + mainClass + ' .card-tabs-stack').children('div[data-tab]').each(function () {
+    $(selector + ' .card-tabs-stack').children('div[data-tab]').each(function () {
     	if($(this).hasClass('active')){
-    		$('.' + mainClass + " .card-tabs-bar a[data-tab='" + $(this).data('tab') + "']").addClass('active');
+    		$(selector + " .card-tabs-bar a[data-tab='" + $(this).data('tab') + "']").addClass('active');
     		toggleTab($(this));
     		$(this).removeClass('active');
     		activeCount++;
@@ -49,11 +50,11 @@ jQuery.fn.cardTabs = function(options){
 
 	// Otherwise, it's the first one, and the first tab in the bar needs to be active
 	if(activeCount == 0){
-		$('.' + mainClass + ' .card-tabs-bar a:first-child').addClass('active');
+		$(selector + ' .card-tabs-bar a:first-child').addClass('active');
 	}
 
-	$('.' + mainClass + ' .card-tabs-bar a').click(function(){
-		$('.' + mainClass + ' .card-tabs-bar a').removeClass('active');
+	$(selector + ' .card-tabs-bar a').click(function(){
+		$(selector + ' .card-tabs-bar a').removeClass('active');
 		$(this).addClass('active');
 		toggleTab($(this));
 	});
